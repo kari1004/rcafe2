4dab96afbafc30b9a89a6b27fda1b8027e04ea35
sukju-kwon <sukju-kwon@HW-0158.local>
2017-08-14 10:11:07 +0900

    add to gemfile : bootstrap-sass & simple form

--- Gemfile
+++ Gemfile
@@ -33,7 +33,7 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
-gem 'bootstrap-sass', '~> 3.3.6'
+gem 'bootstrap-sass'
 gem 'simple_form'
 
 group :development, :test do
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require bootstrap-sprocket
+//= require bootstrap-sprockets
 //= require_jquery_ujs
 //= require rails-ujs
 //= require turbolinks
